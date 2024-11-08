00819a65f5129632269b665afc3b526782107370
Michal Predotka <mivoligo@gmail.com>
2024-11-08 08:52:01 +0100

    Add basic image animation

--- lib/widgets/illustration_part.dart
+++ lib/widgets/illustration_part.dart
@@ -19,6 +19,23 @@ class IllustrationPart extends StatelessWidget {
     5: 'line-5.png',
   };
 
+  static const foregroundImages = {
+    0: '0-f.png',
+    1: '1-f.png',
+    2: '2-f.png',
+    3: '3-f.png',
+    4: '4-f.png',
+    5: '5-f.png',
+  };
+  static const backgroundImages = {
+    0: '0-b.png',
+    1: '1-b.png',
+    2: '2-b.png',
+    3: '3-b.png',
+    4: '4-b.png',
+    5: '5-b.png',
+  };
+
   @override
   Widget build(BuildContext context) {
     return PageView.builder(
@@ -29,6 +46,8 @@ class IllustrationPart extends StatelessWidget {
         return PartOne(
           progress: index,
           imageAsset: imageAssets[index],
+          foregroundImage: foregroundImages[index]!,
+          backgroundImage: backgroundImages[index]!,
         );
       },
       // children: [...children.sublist(0, children.length - 1), PartOne(progress: 4  ?? 0,)],
@@ -37,38 +56,72 @@ class IllustrationPart extends StatelessWidget {
 }
 
 class PartOne extends StatelessWidget {
-  const PartOne({required this.progress, this.imageAsset, super.key});
+  const PartOne({
+    required this.progress,
+    required this.foregroundImage,
+    required this.backgroundImage,
+    this.imageAsset,
+    super.key,
+  });
 
   final int progress;
   final String? imageAsset;
+  final String foregroundImage;
+  final String backgroundImage;
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(color: Colors.redAccent.withOpacity(0.5)),
-        if (imageAsset != null)
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Image.asset(
-              'assets/images/$imageAsset',
-              fit: BoxFit.fitWidth,
-              width: double.infinity,
-            ),
-          ),
-        TweenAnimationBuilder(
-          tween: Tween(begin: 16.0, end: 48.0),
-          duration: const Duration(milliseconds: 1000),
-          builder: (context, value, child) {
-            return Center(
-              child: Text(
-                'data',
-                style: TextStyle(fontSize: value),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Stack(
+          children: [
+            if (imageAsset != null)
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Image.asset(
+                  'assets/images/$imageAsset',
+                  fit: BoxFit.fitWidth,
+                  width: double.infinity,
+                ),
               ),
-            );
-          },
-        ),
-      ],
+            TweenAnimationBuilder(
+              tween: Tween<double>(
+                begin: -constraints.maxWidth,
+                end: 0,
+              ),
+              duration: const Duration(milliseconds: 500),
+              builder: (context, value, child) {
+                return Transform.translate(
+                  offset: Offset(value, 0),
+                  child: child,
+                );
+              },
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Image.asset('assets/images/$backgroundImage'),
+              ),
+            ),
+            TweenAnimationBuilder(
+              tween: Tween<double>(
+                begin: constraints.maxWidth,
+                end: 0,
+              ),
+              duration: const Duration(milliseconds: 500),
+              curve: Curves.ease,
+              builder: (context, value, child) {
+                return Transform.translate(
+                  offset: Offset(value, 0),
+                  child: child,
+                );
+              },
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Image.asset('assets/images/$foregroundImage'),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
